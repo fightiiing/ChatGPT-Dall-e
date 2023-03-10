b49dded0a515c81ce032dee1c906a9301a5a8eae
Mike Giordano <pitiiing@gmail.com>
2023-03-09 22:14:42 -0500

    fully functioning chatgpt API app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,94 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
+import { MainContainer, ChatContainer, MessageList, Message, MessageInput, TypingIndicator } from "@chatscope/chat-ui-kit-react";
+
+const api_key='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';
 
 function App() {
+  const [typing, setTyping] = useState(false);
+  const [messages, setMessages] = useState([
+    {
+      message: "Hi, I'm ChatGPT.",
+      sender: "ChatGPT"
+    }
+  ]);
+
+  const handleSend = async (message) => {
+    const newMessage = {
+          message: message,
+          sender: "user",
+          direction: "outgoing"
+    }
+  
+
+  const newMessages = [...messages, newMessage];
+
+  //update message state
+  setMessages(newMessages);
+  // set a typing indicator for ChatGPT
+  setTyping(true);
+    await processMessageToChatGPT(newMessages);
+}
+
+async function processMessageToChatGPT(chatMessages) {
+  let apiMessages = chatMessages.map((messageObject)=> {
+    let role = "";
+    if (messageObject.sender === "ChatGPT") {
+      role="assistant"
+    } else {
+      role="user"
+    }
+    return {role: role, content: messageObject.message}
+  });
+
+const systemMessage = {
+  role: "system",
+  content: "Explain all concepts like I am 10 years old."
+}
+
+  const apiRequestBody = {
+    "model": "gpt-3.5-turbo",
+    "messages": [systemMessage, ...apiMessages]
+  }
+
+  await fetch("https://api.openai.com/v1/chat/completions", {
+    method: "POST", 
+    headers: {
+      "Authorization": "Bearer " + api_key,
+      "Content-Type": "application/json"
+    }, 
+    body: JSON.stringify(apiRequestBody)
+  }).then((data)=>{
+    return data.json();
+  }).then((data) => {
+   setMessages(
+    [...chatMessages, {
+      message: data.choices[0].message.content,
+      sender: "ChatGPT"
+    }]
+   );
+   setTyping(false);
+  })
+}
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='chat'>
+        <MainContainer>
+          <ChatContainer>
+            <MessageList
+            autoScrollToBottomOnMount={true}
+                typingIndicator={typing ? <TypingIndicator content="ChatGPT is typing"/> : null}
+               >
+              {messages.map((message,i)=>{
+                return <Message key={i} model={message} />
+              })}
+              </MessageList>
+            <MessageInput placeholder='Type message here' onSend={handleSend} ></MessageInput>
+          </ChatContainer>
+        </MainContainer>
+        </div>
     </div>
   );
 }
-
 export default App;
